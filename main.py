871a453a22724ba45b9a8aedf85850f60e508d5d
saotv <161947181+saotv@users.noreply.github.com>
2024-03-02 16:25:18 +0000

    1

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 import streamlit as st
-from streamlit_multipage import MultiPage
+import game1
 
-# 创建一个MultiPage对象
-app = MultiPage()
+# Set the page config
+st.set_page_config(page_title="Game Center", layout="wide")
 
-# 设置页面标题
-st.title('欢迎来到游戏中心')
+# Page navigation
+st.sidebar.title('Navigation')
+selection = st.sidebar.radio("Go to", ['Home', 'Guess the Number Game', 'Game 2', 'Game 3'])
 
-# 导入其他页面的函数
-from game1 import game1_page
-
-# 添加页面到应用
-app.add_page("猜数字游戏", game1_page)
-
-# 你可以继续添加其他游戏的页面
-# from game2 import game2_page
-# app.add_page("游戏2", game2_page)
-
-# from game3 import game3_page
-# app.add_page("游戏3", game3_page)
-
-# 运行应用
-app.run()
+if selection == 'Home':
+    st.title("Welcome to the Game Center")
+    st.write("Please select a game from the navigation menu.")
+elif selection == 'Guess the Number Game':
+    game1.show_game()
+# elif selection == 'Game 2':
+    # Call your game 2 function here
+# elif selection == 'Game 3':
+    # Call your game 3 function here
